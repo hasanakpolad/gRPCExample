e582dafdf4f9ce646397e773e969b9cc378c58a1
hasan akpolad <hasanakpolad@hotmail.com>
2021-02-08 15:13:14 +0300

    Add project files.

---
+++ GrpcExampleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using gRPCExampleService;
+using System;
+using System.Threading;
+
+namespace GrpcExampleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greeter = new Greeter.GreeterClient(channel);
+            HelloReply reply = greeter.SayHello(new HelloRequest
+            {
+                Name = "Hasan's Request Example"
+            });
+
+            Console.WriteLine(reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
